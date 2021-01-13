f1d8aab06dd7bb84d1964e656585d2f54f5f5367
yqq <youngqqcn@163.com>
2021-01-13 17:52:55 +0800

    impl  address generation demo

--- demos/cpp_demos/htdf_address_generate.cpp
+++ demos/cpp_demos/htdf_address_generate.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <string>
+
+#include "htdf/htdf.h"
+#include "htdf/crypto/strencodings.h"
+
+#include <assert.h>
 using namespace std;
 
+void GenerateAddress()
+{
+    string privKey; 
+    privKey.resize(32);
+    htdf::MakeNewKey( (unsigned char *)privKey.data() );
+    string  strPrivKey = Bin2HexStr((unsigned char *)privKey.data(), privKey.size());
+    cout << "private key: " << strPrivKey << endl;
+
+    string pubKey;
+    int ret = htdf::PrivateKeyToCompressPubKey(strPrivKey, pubKey);
+    assert(0 == ret);
+    cout << "public key: " << pubKey << endl;
+    string strAddress = htdf::PubkToAddress(pubKey);
+    cout << "address key: " << strAddress << endl;
+}
 
 
 int main(int argc, char const *argv[])
 {
-    cout << "htdf_address_generate demo" << endl;
+    GenerateAddress();
     return 0;
 }
